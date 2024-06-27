a2488efc82742fa629a05dc97fe5dfc75cc0a3d5
Samuel <amsamgitau@gmail.com>
2024-06-27 08:46:25 +0300

    complete test

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//Multiply
+let num1=31
+let num2=2
+let num3=10
+let num4=6
+const multiply=num1 * num2
 
-// Write your code here
+//Generate Random numbers
+const random = Math.floor(Math.random()*10)+1;
+
+//CALCULATING REMAINDER 
+const mod = num3 % num4
+
+//Get max num
+const max= Math.max(5, 10, 15, 20);
